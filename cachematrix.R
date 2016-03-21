e24be2e5e20233737ddd2262c9ed755021d2abfc
jrtay08 <taylor_john@ymail.com>
2016-03-20 22:49:01 -0700

    ProgrammingAssignment2
    Working submission for R Programming assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Programming Assignment 2 ##
+# makeCacheMatrix caches the inverse of a matrix
+# while chacheSolve checks to see if a matrix inverse
+# has been changed
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    ## "<<" required for global environment so it doesn't overrite values in current environment
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  set_inverse <- function(inverse) inv <<- inverse
+  get_inverse <- function() inv
+  list(set = set, get = get,
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$get_inverse()
+  
+  # Checks if the inverse is already calculated
+  if(!is.null(inv)) {
+    message("getting cached data")
+    inv
+  }
+  
+  # if invers is not calculated, then perform calc
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$set_inverse(inv)
+  inv
 }
+
+
+# using below to test above functions
+# Creating because cache_matrix stored as a function
+# rather than a matrix when not in a function
+check <- function(strd_matrix) {
+  cached_matrix <- makeCacheMatrix(strd_matrix)
+  cacheSolve(cached_matrix)
+  #run a second time to throw "gettinc chached data" message
+  cacheSolve(cached_matrix)
+}
+
+
